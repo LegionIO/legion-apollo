35abd04acc1cfcb8de4b19e3034f093c83a871a8
Esity <matthewdiverson@gmail.com>
2026-03-25 22:56:27 -0500

    add local knowledge settings defaults

--- spec/legion/apollo/settings_spec.rb
+++ spec/legion/apollo/settings_spec.rb
@@ -1,14 +1,37 @@
 # frozen_string_literal: true
 
+require 'spec_helper'
+
 RSpec.describe Legion::Apollo::Settings do
   describe '.default' do
-    it 'returns a hash with required keys' do
-      defaults = described_class.default
-      expect(defaults).to be_a(Hash)
-      expect(defaults[:enabled]).to be true
-      expect(defaults[:default_limit]).to eq(5)
-      expect(defaults[:min_confidence]).to eq(0.3)
-      expect(defaults[:max_tags]).to eq(20)
+    subject(:defaults) { described_class.default }
+
+    it 'includes local settings' do
+      expect(defaults[:local]).to be_a(Hash)
+    end
+
+    it 'enables local by default' do
+      expect(defaults[:local][:enabled]).to be true
+    end
+
+    it 'sets retention to 5 years' do
+      expect(defaults[:local][:retention_years]).to eq(5)
+    end
+
+    it 'defaults query scope to all' do
+      expect(defaults[:local][:default_query_scope]).to eq(:all)
+    end
+
+    it 'sets fts candidate multiplier to 3' do
+      expect(defaults[:local][:fts_candidate_multiplier]).to eq(3)
+    end
+
+    it 'sets default limit to 5' do
+      expect(defaults[:local][:default_limit]).to eq(5)
+    end
+
+    it 'sets min confidence to 0.3' do
+      expect(defaults[:local][:min_confidence]).to eq(0.3)
     end
   end
 end
